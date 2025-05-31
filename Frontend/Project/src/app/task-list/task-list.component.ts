89224bcd581f2f21be1f5879aa616e0f28e3ae92
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-31 20:57:58 +0500

    main structure to task list component

--- Frontend/Project/src/app/task-list/task-list.component.ts
+++ Frontend/Project/src/app/task-list/task-list.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../models';
+import { TaskService } from '../services/task.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit{
+  tasks: Task[] = [];
+  isLoading = true;
+  constructor(
+    private taskService: TaskService,
+    private router: Router
+  ){}
 
+  ngOnInit(): void {
+      this.loadTasks();
+
+  }
+
+  loadTasks(): void {
+    this.isLoading = true;
+    this.taskService.getTasks().subscribe({
+      next: (tasks) => {
+        this.tasks = tasks;
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error loading tasks', err);
+        this.isLoading = false;
+      }
+    })
+  }
+
+  viewTaskDetail(id: number): void{
+    this.router.navigate(['/tasks', id]);
+  }
+  
 }
