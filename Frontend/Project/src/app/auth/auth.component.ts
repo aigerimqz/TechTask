ca4b230107ded851b2eeee52a19788767dee9122
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-31 21:12:51 +0500

    changes in auth comp

--- Frontend/Project/src/app/auth/auth.component.ts
+++ Frontend/Project/src/app/auth/auth.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { User } from '../../models';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  userModel: User;
+  errorMessage: string = '';
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ){
+    this.userModel = {} as User;
+  }
+
+  onLogin(): void{
+    this.authService.login(this.userModel).subscribe({
+      next: (token) => {
+        localStorage.setItem('access', token.access);
+        localStorage.setItem('refresh', token.refresh);
+        this.router.navigate(['/']);
+        
+      },
+      error: () => {
+        this.errorMessage = 'Wrong username or password';
+      }
+    })
+  }
 }
