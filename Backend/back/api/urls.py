1f859c5829b5eb81fd096c1e486a2a51ff01930c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-31 20:14:41 +0500

    also created views serializers urls for completely work of backend

--- Backend/back/api/urls.py
+++ Backend/back/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import *
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tasks/', TaskListCreateView.as_view(), name='tasks'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail')
 ]
--- Backend/back/api/views.py
+++ Backend/back/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
 # Create your views here.
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        
+        return Task.objects.filter(user=self.request.user)
