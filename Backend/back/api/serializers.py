6d01e3c8983fbe062252e77aadf2220abf71efe4
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-31 21:47:19 +0500

    added register serializer

--- Backend/back/api/serializers.py
+++ Backend/back/api/serializers.py
@@ -1,7 +1,36 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only = True,
+        required = True,
+        validators = [validate_password]
+    )
+    password2 = serializers.CharField(write_only = True, required = True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        
+        return user
